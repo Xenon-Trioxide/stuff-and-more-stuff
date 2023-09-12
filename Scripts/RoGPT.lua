a301eaa7b564b388a597f916dbf7bb6fb93e5bb9
Xenon-Trioxide <141217057+Xenon-Trioxide@users.noreply.github.com>
2023-09-12 20:51:52 +0700

    Create RoGPT.lua

---
+++ Scripts/RoGPT.lua
@@ -0,0 +1,2 @@
+local gptlibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenon-Trioxide/stuff-and-more-stuff/main/ChatGPT.lua"))()
+-- prob gonna add more when i release the new library
