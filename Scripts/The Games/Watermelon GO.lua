9531199afe74187d1f9afb77d8ae084300bcae35
Xenon-Trioxide <141217057+Xenon-Trioxide@users.noreply.github.com>
2024-08-03 16:33:33 +0700

    Create Watermelon GO.lua

---
+++ Scripts/The Games/Watermelon GO.lua
@@ -0,0 +1,74 @@
+local module = {}
+
+function module:LoadShines(plr, window, tab, SERVICES, EVENTS)
+  local toggle = false
+  local found = false
+
+  tab:Label("Enable this in the main menu!")
+  tab:Toggle({
+    Name = "Show all shines",
+    Callback = function(val)
+      toggle = val
+      if toggle then repeat
+        for i,v in ipairs(plr.PlayerGui.MainUI:GetDescendants()) do
+        	if string.find(v.Name,"FAKE_ITEM") and v.Visible then
+            found = true
+            
+        		v.Parent = plr.PlayerGui.MainUI
+        		v.Size = UDim2.new(0.5, 0, 0.5, 0)
+        		v.Position = UDim2.new(0.5, 0, 0.5, 0)
+        		v.BackgroundTransparency = 0
+        		v.ZIndex = 10000000
+            v.Activated:Wait()
+            v:Destroy()
+        	end
+        end
+        if not found then
+          toggle = false
+        else
+          found = false
+        end
+        wait(0.1)
+      until not toggle end
+    end
+  })
+
+end
+
+function module:LoadSilver(plr, window, tab, SERVICES, EVENTS)
+  toggle = false
+  
+  tab:Label("Make sure you are in a match before enabling this!")
+  tab:Label("After you enable just spam click and watch your score go up!")
+  tab:Toggle({
+      Name = "Auto merge balls",
+      Callback = function(val)
+        toggle = val
+        if toggle then
+          workspace.Playspaces.Singleplayer.LocalBoxParts.LeftWall.CanCollide = false
+          workspace.Playspaces.Singleplayer.LocalBoxParts.RightWall.CanCollide = false
+          
+          EVENTS["ChildAdded1"] = workspace.Playspaces.Duels.ActiveFruits.ChildAdded:Connect(function()
+          	for i,v in pairs(workspace.Playspaces.Duels.ActiveFruits:GetChildren()) do
+          		if v:IsA("BasePart") then
+          			v.CFrame = CFrame.new(-46.36000061035156, 33.966381072998047, 97.6836929321289)
+          		end
+          	end
+          end)
+          EVENTS["ChildAdded2"] = workspace.Playspaces.Singleplayer.ActiveFruits.ChildAdded:Connect(function()
+          	for i,v in pairs(workspace.Playspaces.Singleplayer.ActiveFruits:GetChildren()) do
+          		if v:IsA("BasePart") then
+          			v.CFrame = CFrame.new(-46.36000061035156, 33.966381072998047, 97.6836929321289)
+          		end
+          	end
+          end)
+        else
+          for i,v in pairs(EVENTS) do
+            v:Disconnect()
+          end
+        end
+      end
+  })
+end
+
+return module
